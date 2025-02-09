d2195dea747b9af7007438788716c73315fcefe9
armmmartirosyan <106014381+armmmartirosyan@users.noreply.github.com>
2025-02-09 21:26:13 +0400

    fixed scroll issue

--- src/components/album/index.js
+++ src/components/album/index.js
@@ -14,8 +14,8 @@ export function Album() {
             <AlbumItem album={item} />
           </div>
         ))}
-        <div className={styles.empty} />
       </div>
+      <div className={styles.empty} />
     </section>
   );
 }
