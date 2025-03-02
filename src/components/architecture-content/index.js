605bfe8d336852b77cfb03dbbc86a77f72f2e8c9
armmmartirosyan <106014381+armmmartirosyan@users.noreply.github.com>
2025-03-02 06:25:16 +0400

    architecture. scrooll fix

--- src/components/architecture-content/index.js
+++ src/components/architecture-content/index.js
@@ -5,43 +5,45 @@ import styles from "./index.module.scss";
 
 export function ArchitectureContent({ project }) {
   return (
-    <div className={styles.content}>
-      <div className={styles.left_part}>
-        <div className={styles.left_part_container}>
-          <h1 className={styles.title}>Architecture</h1>
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <div className={styles.left_part}>
+          <div className={styles.left_part_container}>
+            <h1 className={styles.title}>Architecture</h1>
 
-          <div className={styles.text_container}>
-            <p className={styles.project_title}>{project.name}</p>
-            <p className={styles.project_location}>
-              <FontAwesomeIcon
-                icon={faLocationDot}
-                className={styles.location_icon}
-              />
-              {project.location}
-            </p>
-            <p className={styles.project_buildinng_type}>
-              <FontAwesomeIcon
-                icon={faBuilding}
-                className={styles.building_icon}
-              />
-              {project.buildingType}
-            </p>
+            <div className={styles.text_container}>
+              <p className={styles.project_title}>{project.name}</p>
+              <p className={styles.project_location}>
+                <FontAwesomeIcon
+                  icon={faLocationDot}
+                  className={styles.location_icon}
+                />
+                {project.location}
+              </p>
+              <p className={styles.project_buildinng_type}>
+                <FontAwesomeIcon
+                  icon={faBuilding}
+                  className={styles.building_icon}
+                />
+                {project.buildingType}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.right_part}>
-        {project.images.map((image, index) => (
-          <div className={styles.image_wrapper} key={index}>
-            <Image
-              loading="eager"
-              priority
-              quality={60}
-              src={image.src}
-              alt={image.alt}
-              className={styles.image}
-            />
-          </div>
-        ))}
+        <div className={styles.right_part}>
+          {project.images.map((image, index) => (
+            <div className={styles.image_wrapper} key={index}>
+              <Image
+                loading="eager"
+                priority
+                quality={60}
+                src={image.src}
+                alt={image.alt}
+                className={styles.image}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
