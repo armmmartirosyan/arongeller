df808d704f7c48a2a1f3b004d4fb9d03b5efcf09
armmmartirosyan <106014381+armmmartirosyan@users.noreply.github.com>
2025-03-02 20:26:25 +0400

    fix

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -2,17 +2,19 @@ import styles from "./index.module.scss";
 
 export function Banner() {
   return (
-    <section className={styles.banner}>
-      <h2 className={styles.title}>
-        Building <br />A <span className={styles.highlighted}>Better</span>{" "}
-        <br />
-        Tomorrow.
-      </h2>
-      <p className={styles.text}>
-        Where artistry meets engineering. We combine a passion for design with
-        meticulous craftsmanship to create structures that stand the test of
-        time.
-      </p>
-    </section>
+    <div className={styles.banner_wrapper}>
+      <section className={styles.banner}>
+        <h2 className={styles.title}>
+          Building <br />A <span className={styles.highlighted}>Better</span>{" "}
+          <br />
+          Tomorrow.
+        </h2>
+        <p className={styles.text}>
+          Where artistry meets engineering. We combine a passion for design with
+          meticulous craftsmanship to create structures that stand the test of
+          time.
+        </p>
+      </section>
+    </div>
   );
 }
