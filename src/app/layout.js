6117c002ef582a7fb82027e82a414dbca4c9e1c5
armmmartirosyan <106014381+armmmartirosyan@users.noreply.github.com>
2025-03-05 19:47:57 +0400

    added metadata info

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,25 @@ import { allFontsVariables } from "@fonts";
 import "@styles/globals.scss";
 
 export const metadata = {
-  title: "Aron Geller",
-  description: "",
+  title: "Architect Portfolio | Aron Geller",
+  description:
+    "Discover breathtaking, high-end villa designs crafted by a dedicated architect. Combining strength, elegance, and functionality, each project is meticulously planned to create a perfect living space for families.",
+  keywords: [
+    "architect",
+    "luxury villas",
+    "modern architecture",
+    "high-end homes",
+    "family-friendly design",
+    "strong and beautiful buildings",
+    "custom villa designs",
+    "Aron Geller",
+    "Aron",
+    "Geller",
+    "design",
+    "luxury",
+    "home",
+    "house",
+  ],
 };
 
 export default function RootLayout({ children }) {
